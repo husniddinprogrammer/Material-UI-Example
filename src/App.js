9cdfb4c60542a024b0e1ee63652ab972b5569bcd
husniddinprogrammer <husniddinprogrammer@gmail.com>
2026-02-10 23:37:14 +0500

    add navbar and app-bar

--- src/App.js
+++ src/App.js
@@ -1,96 +1,93 @@
+import React from 'react';
 import { AvatarGroup, Badge, Button, ButtonGroup, Divider } from '@mui/material';
-import YoutubeSearchedForTwoTone from '@mui/icons-material/YoutubeSearchedForTwoTone';
-import ZoomIn from '@mui/icons-material/ZoomIn';
-import SaveIcon from '@mui/icons-material/Save';
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import Avatar from '@mui/material/Avatar';
-import AssignmentIcon from '@mui/icons-material/Assignment';
 import MailIcon from '@mui/icons-material/Mail';
-import FormatBoldIcon from '@mui/icons-material/FormatBold';
-import FormatItalicIcon from '@mui/icons-material/FormatItalic';
-
-
+import { Box, IconButton, Typography } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function App() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
       <div>
-        {/* //Buttons example */}
-        <h1>Buttons</h1>
-        <Button variant="text" color='dark'>Text</Button>
-        <Button variant="contained" color='warning'>Contained</Button>
-        <Button variant="outlined" color='success'>Outlined</Button>
-        <br />
-        <Button color='primary'>Primary</Button>
-        <Button disabled>Disabled</Button>
-        <Button href="#text-buttons" color='info'>Link</Button>
-        <br />
-        <Button variant="contained" disableElevation color='secondary'>
-          Disable elevation
-        </Button>
-        <br />
-        <Button size="small">Small</Button>
-        <Button size="medium">Medium</Button>
-        <Button size="large">Large</Button>
-        <br />
-        <Button startIcon={<span>👍</span>}>Like</Button>
-        <Button endIcon={<span>👍</span>}>Dislike</Button>
-        <br />
-        {/* Button Group */}
-        <h1>Button Group</h1>
-        <ButtonGroup variant="contained" aria-label="outlined primary button group">
-          <Button>Left</Button>
-          <Button>Middle</Button>
-          <Button>Right</Button>
-        </ButtonGroup>
-        <br />
-        <ButtonGroup variant="outlined" aria-label="Loading button group">
-          <Button>Submit</Button>
-          <Button>Fetch data</Button>
-          <Button loading loadingPosition="start" startIcon={<SaveIcon />}>
-            Save
-          </Button>
-        </ButtonGroup>
-        <br />
-        {/* Icons Example */}
-        <h1>Icons Exaple</h1>
-        <YoutubeSearchedForTwoTone />
-        <ZoomIn color="action" />
-        <AccessAlarmIcon color="primary" sx={{ fontSize: 40 }} />
+        {/* 1 Example */}
+        <AppBar component="nav">
+          <Toolbar>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              MUI
+            </Typography>
+            <Box>
+              <Button key="LISTING" sx={{ color: '#fff' }}>
+                LISTING
+              </Button>
+              <Button key="MENTORS" sx={{ color: '#fff' }}>
+                MENTORS
+              </Button>
+              <Button key="MY ACCOUNT" sx={{ color: '#fff' }}>
+                MY ACCOUNT
+              </Button>
+              <Button key="LOG OUT" sx={{ color: '#fff' }}>
+                LOG OUT
+              </Button>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <AppBar position="static" sx={{ backgroundColor: "#00bcd4" , marginTop: "200px" }}>
+          <Toolbar>
+            {/* Left */}
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              Navbar
+            </Typography>
 
-        {/* Data Display Exaple */}
-        <h1>Data Display Exaple</h1>
-        {/* Avatar */}
-        <Avatar alt="Example" src="https://mui.com/static/images/avatar/1.jpg" />
-        <Avatar sx={{ bgcolor: 'primary.light', width: 40, height: 40 }}>JS</Avatar>
-        <Avatar variant="square" sx={{ width: 40, height: 40 }}>
-          <AssignmentIcon />
-        </Avatar>
-        <AvatarGroup max={4} total={6}>
-          <Avatar alt="Mahmudov Husniddin" src="https://mui.com/static/images/avatar/1.jpg" />
-          <Avatar alt="Mahmudov Javlon" src="https://mui.com/static/images/avatar/2.jpg" />
-          <Avatar alt="Jurayeva Gulnoza" src="https://mui.com/static/images/avatar/3.jpg" />
-          <Avatar alt="Mahmudov Husniddin" src="https://mui.com/static/images/avatar/1.jpg" />
-          <Avatar alt="Mahmudov Javlon" src="https://mui.com/static/images/avatar/2.jpg" />
-          <Avatar alt="Jurayeva Gulnoza" src="https://mui.com/static/images/avatar/3.jpg" />
-        </AvatarGroup>
+            {/* Right */}
+            <Button color="inherit" startIcon={<MailIcon />}>
+              Contact
+            </Button>
 
-        {/* Badge Example */}
-        <h1>Badge Example</h1>
-        <Badge badgeContent={4} color="primary">
-          <MailIcon color="action" />
-        </Badge>
-        <Badge color="secondary" badgeContent={0} showZero>
-          <MailIcon />
-        </Badge>
-        <Badge color="secondary" badgeContent={1000} max={999}>
-          <MailIcon />
-        </Badge>
-        {/* Divider Example */}
-        <h1>Divider Example</h1>
-        <FormatBoldIcon />
-        <Divider orientation="vertical" variant="middle" flexItem />
-        <FormatItalicIcon />
+            <Button color="inherit" startIcon={<SettingsIcon />}>
+              Settings
+            </Button>
+
+            <IconButton color="inherit" onClick={handleMenuOpen}>
+              <AccountCircle />
+              <Typography sx={{ ml: 1 }}>Profile</Typography>
+            </IconButton>
+
+            {/* Dropdown */}
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+            >
+              <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+              <MenuItem onClick={handleMenuClose}>Log out</MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
       </div>
     </>
   );
